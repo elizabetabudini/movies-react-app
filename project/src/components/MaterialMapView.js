f12002501324c3e07ea0e55a410b0555ef0edb52
elizabetabudini <elizabeta.budini@studio.unibo.it>
2020-04-18 00:44:49 +0100

    logout working-drawer menu

--- project/src/components/MaterialMapView.js
+++ project/src/components/MaterialMapView.js
@@ -23,20 +23,10 @@ class MaterialMapView extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
     alignItems: 'center',
   },
   mapView1: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    flex: 1,
   },
 });
 
